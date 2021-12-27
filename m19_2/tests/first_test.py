7ec09ceeb67e5f5aa45b481b5c9420da1b103fbc
Evgeniya9211 <87281314+Evgeniya9211@users.noreply.github.com>
2021-12-28 00:21:05 +0300

    Модуль 19,2
    Добавила исходники практического задания в модуле 19.2

---
+++ m19_2/tests/first_test.py
@@ -0,0 +1,14 @@
+import pytest
+from app.calculator import Calculator
+
+
+class TestCalc:
+    def setup(self):
+        self.calc = Calculator
+
+    def test_multiply_calculate_correctly(self):
+        assert self.calc.multiply(self, 2, 2) == 4
+
+    def test_multiply_failed(self):
+        assert self.calc.multiply(self, 2, 2) == 5
+
